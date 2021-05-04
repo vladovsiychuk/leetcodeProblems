27fe5cee9dc2d5971f72b4731bca79bb813b2956
Kepva <vladyslav.ovsiychuk@gmail.com>
2021-05-04 12:26:03 +0200

    204

--- src/count_primes_204/My.java
+++ src/count_primes_204/My.java
@@ -2,6 +2,28 @@ package count_primes_204;
 
 public class My {
     public static int result(int n) {
-        return 1;
+
+        if (n < 2)
+            return 0;
+
+        int res = 0;
+        boolean notPrimes[] = new boolean[n];
+
+        for(int i=2; i < n; i++) {
+            if (i*2 >= n)
+                break;
+
+            if(!notPrimes[i]) {
+                for(int j=i*2; j < n; j = j+i) {
+                    notPrimes[j] = true;
+                }
+            }
+        }
+
+        for(int i = 2; i < n; i++)
+            if (notPrimes[i] == false)
+                res++;
+
+        return res;
     }
 }
--- src/count_primes_204/main.java
+++ src/count_primes_204/main.java
@@ -4,6 +4,6 @@ public class main {
     public static void main(String args[]){
 //        System.out.println(Solution.countPrimes(20));
 //        System.out.println(Solution.countPrimes2(20));
-        System.out.println(My.result(20));
+        System.out.println(My.result(10));
     }
 }
